096239a1625e70fc260166bf08b7e07972b9682d
Oskar Ortiz <okyjulian994@gmail.com>
2026-05-07 13:14:05 -0500

    Modificaciones Listado de Reservas

--- reservas_admin/models.py
+++ reservas_admin/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
 from django.db.models import Q
 
 
